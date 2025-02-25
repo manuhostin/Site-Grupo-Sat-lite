260f91c3bb01ffcf9df6630b364f4b07e9cd5bb6
manuhostin <hostimmanu@gmail.com>
2025-02-25 19:32:41 -0300

    feat: botao wpp

---
+++ src/components/BtnWhatsapp.vue
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<template>
+
+<div class="zapbutton">
+
+<a href="https://wa.me/+5547996770009" target="_blank">
+    <img src="../assets/zap.png" width="50" alt="Botão de contato" title="Botão de contato ">
+</a>
+</div>
+
+
+
+</template>
+
+<style>
+.zapbutton{
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+    z-index: 999;
+}
+.zapbutton:hover {
+  transform: scale(1.1);
+
+}
+</style>
